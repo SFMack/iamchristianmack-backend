77ca71935b501cfa8d440ede1011c82fbf3d2d92
SFMack <sfcamack@icloud.com>
2021-01-31 18:37:31 -0800

    added cors as middleware

--- index.js
+++ index.js
@@ -13,26 +13,17 @@ const app = express();
 
 app.use(cors());
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://iamchristianmack.com");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.send("We lit");
 });
 
-app.post("/api/send", (req, res) => {
+app.post("/api/send", cors(), (req, res) => {
   const data = req.body;
-  console.log(data);
-  console.log(process.env.user);
+
+  req.headers;
 
   const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
