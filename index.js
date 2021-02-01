5b9f3eeaeff5dd021b620ad241de383926838240
SFMack <sfcamack@icloud.com>
2021-01-31 18:45:23 -0800

    added bodyParser back

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const app = express();
 
 app.use(cors());
 
-// app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.send("We lit");
